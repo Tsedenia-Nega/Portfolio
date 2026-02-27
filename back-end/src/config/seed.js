1cbdd3356bed68269196516293712f1e3674a524
Tsedenia <tsedenianega@gmail.com>
2026-02-27 09:20:34 +0300

    seed

--- back-end/src/config/seed.js
+++ back-end/src/config/seed.js
@@ -1,22 +1,32 @@
 import bcrypt from "bcrypt";
 import User from "../models/userModel.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const seedAdmin = async () => {
   try {
+    const adminEmail = process.env.ADMIN_EMAIL;
+    const adminPassword = process.env.ADMIN_PASSWORD;
 
-    const adminExists = await User.findOne({ email: "tsedenia301@gmail.com" });
+    // Safety check: Make sure the env variables actually exist
+    if (!adminEmail || !adminPassword) {
+      console.error("Missing ADMIN_EMAIL or ADMIN_PASSWORD in .env file");
+      return;
+    }
+
+    const adminExists = await User.findOne({ email: adminEmail });
 
     if (!adminExists) {
-      await User.deleteMany({}); 
-      const hashedPassword = await bcrypt.hash("Goodfuture94!", 10);
+      const hashedPassword = await bcrypt.hash(adminPassword, 10);
 
       await User.create({
-        email: "tsedenia301@gmail.com",
+        email: adminEmail,
         password: hashedPassword,
       });
-      console.log(" Admin seeded successfully!");
+      console.log("Admin seeded successfully!");
     } else {
-      console.log(" Admin already exists, skipping seed.");
+      console.log("Admin already exists, skipping seed.");
     }
   } catch (error) {
     console.error(" Seeding error:", error.message);
